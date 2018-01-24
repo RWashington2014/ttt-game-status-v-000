1724f20343e716f22874c6dda085e009d04d54de
Robert W <robwash@gmail.com>
2018-01-24 14:24:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,21 +7,13 @@ end
 WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 def won?(board)
-
-  win_combo = []
-
-  win_combo = WIN_COMBINATIONS.each do |win_array|
-    win_combo << win_array
+  WIN_COMBINATIONS.find do |win_array|
+    board[win_array[0]] == board[win_array[1]] && board[win_array[1]] == board[win_array[2]] && position_taken?(board, win_array[0])
   end
-  
+end
 
-  WIN_COMBINATIONS.any? { |combo| combo == true  }
+def full?(board)
 
-
-
-  #WIN_COMBINATIONS.select do |win_array|
-  #   puts win_array.inspect
-   #end
-
+  board.all
 
 end
